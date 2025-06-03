92a1828536c43de81a21083be7da21535e94473b
Hannes Olschok <hannes.o@t-online.de>
2025-06-03 10:20:47 +0200

    changed index to module (initializing the window.projectmanager)

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+import { initializeData } from "./init.js";
+
 let lastProjects = [];
 
 function initFilter() {
     lastProjects = [...window['projects']]
-    .sort((a, b) => new Date(b.end_date) - new Date(a.end_date))
-    .slice(0, 3);
+        .sort((a, b) => new Date(b.end_date) - new Date(a.end_date))
+        .slice(0, 3);
 }
 
 function showLastProjects() {
     const projectContainer = document.getElementById('lastProjectView');
     projectContainer.innerHTML = '';
-    
+
     lastProjects.forEach(project => {
         const article = document.createElement('article');
-        
+
         const h2 = document.createElement('h2');
         const a = document.createElement('a');
         a.href = `projectView/projectView.html?id=${project.id}`;
         a.textContent = project.title;
         h2.appendChild(a);
-        
+
         const p = document.createElement('p');
         p.textContent = project.short_description;
-        
+
         article.appendChild(h2);
         article.appendChild(p);
         projectContainer.appendChild(article);
@@ -39,6 +41,11 @@ function startSearch(searchTxt) {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  initFilter();
-  showLastProjects();
+    initializeData(window['projects'], []);
+
+    initFilter();
+    showLastProjects();
 });
+
+
+window.startSearch = startSearch;
